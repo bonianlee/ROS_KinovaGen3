4f3d4e40a119b0cdd30cb4f3e85fa89af099bf9d
bonianlee <ccasdqwe@gmail.com>
2024-06-16 22:01:56 +0800

    Fix the clock missing

--- src/mobile_manipulator_speed_tracking.cpp
+++ src/mobile_manipulator_speed_tracking.cpp
@@ -20,8 +20,10 @@ bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub
     cout << "--------- Platform Mode ----------" << endl;
     ros::Rate loop_rate(10);
     bool return_status = true;
-    int64_t t_start = GetTickUs(), now = GetTickUs();    // Unit: microsecond
-    double exp_time = (double)(now - t_start) / 1000000; // Unit: second
+    // clock
+    int64_t t_start = GetTickUs(), now = GetTickUs(), last = now;    // Unit: microsecond
+    double exp_time = (double)(now - t_start) / 1000000, dt; // Unit: second
+    
     double position_curr_p; // 移動平台當前旋轉方向 -pi~pi
     int round_p;            // 移動平台旋轉圈數
     Matrix<double> q_p(3, 1);   // q_p = [x_p; y_p; phi_p] all elements are -inf~inf
@@ -51,54 +53,70 @@ bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub
     dq_pd[2] = 0.5;
 
     Matrix<double> error_p = q_p - q_pd;
+    Matrix<double> error_p_tf(3, 1);
 
     while (ros::ok())
     {
         if (!_kbhit())
         {
-            lee::reference_cmd_vel(dq_pd, q_p, round_p, cmd_vel_r);
-            lee::mobile_platform_error_tf(q_pd, q_p, position_curr_p, error_p);
-            lee::mobile_platform_control_rule(q_pd, error_p, cmd_vel);
-            // Update time, differentiation, and integration
-            prev_q_p = q_p;
-
-            // Updat the platform's data
-            // for translation data
-            for (unsigned int i = 0; i < 2; i++)
-                q_p[i] = platformState.q_p[i];
-            // for rotated data
-            position_curr_p = platformState.q_p[2];
-            q2inf_p(position_curr_p, prev_q_p, round_p, q_p);
-
-            if (exp_time < 50)
+            now = GetTickUs();
+            if (now - last > 1000)
             {
-                q_pd[0] = 0.2 * cos(0.1 * exp_time) + q_pd0[0];
-                q_pd[1] = 0.2 * sin(0.1 * exp_time) + q_pd0[1];
-                q_pd[2] = 0.5 * exp_time + q_pd0[2];
+                lee::reference_cmd_vel(dq_pd, q_p, round_p, cmd_vel_r);
+                lee::mobile_platform_error_tf(error_p, position_curr_p, error_p_tf);
+                lee::mobile_platform_control_rule(q_pd, error_p_tf, cmd_vel);
+                // Update time, differentiation, and integration
+                now = GetTickUs();
+                exp_time = (double)(now - t_start) / 1000000;
+                dt = (double)(now - last) / 1000000;
+                prev_q_p = q_p;
 
-                dq_pd[0] = -0.02 * sin(0.1 * exp_time);
-                dq_pd[1] = 0.02 * cos(0.1 * exp_time);
-                dq_pd[2] = 0.5;
+                // Updat the platform's data
+                // for translation data
+                for (unsigned int i = 0; i < 2; i++)
+                    q_p[i] = platformState.q_p[i];
+                // for rotated data
+                position_curr_p = platformState.q_p[2];
+                q2inf_p(position_curr_p, prev_q_p, round_p, q_p);
+
+                if (exp_time < 50)
+                {
+                    q_pd[0] = 0.2 * cos(0.1 * exp_time) + q_pd0[0];
+                    q_pd[1] = 0.2 * sin(0.1 * exp_time) + q_pd0[1];
+                    q_pd[2] = 0.5 * exp_time + q_pd0[2];
+
+                    dq_pd[0] = -0.02 * sin(0.1 * exp_time);
+                    dq_pd[1] = 0.02 * cos(0.1 * exp_time);
+                    dq_pd[2] = 0.5;
+                }
+                else
+                {
+                    q_pd[0] = 0.2 * cos(0.1 * 50) + q_pd0[0];
+                    q_pd[1] = 0.2 * sin(0.1 * 50) + q_pd0[1];
+                    q_pd[2] = 0.5 * 50 + q_pd0[2];
+
+                    dq_pd[0] = 0;
+                    dq_pd[1] = 0;
+                    dq_pd[2] = 0;
+                }
+
+                error_p = q_p - q_pd;
+
+                // debug----------------------------
+                cout << "--------------------------" << endl;
+                cout << "error_p:";
+                for (unsigned int i = 0; i < 3; i++)
+                    cout << error_p[i] << ",";
+                cout << endl;
+                // debug----------------------------
+                
+                // ROS Publisher
+                geometry_msgs::Twist twist;
+                lee::admittance2platformVel(cmd_vel, twist);
+                platform_pub.publish(twist);
+                ros::spinOnce();
+                loop_rate.sleep();
             }
-            else
-            {
-                q_pd[0] = 0.2 * cos(0.1 * 50) + q_pd0[0];
-                q_pd[1] = 0.2 * sin(0.1 * 50) + q_pd0[1];
-                q_pd[2] = 0.5 * 50 + q_pd0[2];
-
-                dq_pd[0] = 0;
-                dq_pd[1] = 0;
-                dq_pd[2] = 0;
-            }
-
-            error_p = q_p - q_pd;
-
-            // ROS Publisher
-            geometry_msgs::Twist twist;
-            lee::admittance2platformVel(cmd_vel, twist);
-            platform_pub.publish(twist);
-            ros::spinOnce();
-            loop_rate.sleep();
         }
         else if ((char)_getch() == 's')
         {
