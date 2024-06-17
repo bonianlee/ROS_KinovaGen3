27448e4f72a454135bf92f1271154966bbf3d1ea
bonianlee <ccasdqwe@gmail.com>
2024-06-17 23:25:37 +0800

    Add a new topic and a message relates to agv's states

--- src/mobile_manipulator_speed_tracking.cpp
+++ src/mobile_manipulator_speed_tracking.cpp
@@ -7,6 +7,7 @@
 #include "ros/ros.h"
 #include "kinova_test/kinovaMsg.h"
 #include "std_msgs/Bool.h"
+#include "kinova_test/agvMsg.h"
 
 // Custom functions
 #include "kinova_test/KinovaConfig.h"
@@ -15,9 +16,10 @@
 #include "kinova_test/HumanState.h"
 #include "kinova_test/PlatformState.h"
 
-bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub)
+bool platform_control(PlatformState &platformState, ros::Publisher &platform_cmd_pub, ros::Publisher &platform_state_pub)
 {
     cout << "--------- Platform Mode ----------" << endl;
+    kinova_test::agvMsg agvInfo; // ROS-related
     ros::Rate loop_rate(10);
     bool return_status = true;
     // clock
@@ -26,7 +28,7 @@ bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub
     
     double position_curr_p; // 移動平台當前旋轉方向 -pi~pi
     int round_p = 0;            // 移動平台旋轉圈數
-    Matrix<double> q_p(3, 1);   // q_p = [x_p; y_p; phi_p] all elements are -inf~inf
+    Matrix<double> q_p(3, 1), dq_p(3, 1);   // q_p = [x_p; y_p; phi_p] all elements are -inf~inf
     Matrix<double> prev_q_p(3, 1);   // -inf~inf
     Matrix<double> cmd_vel_r(2, 1), cmd_vel(2, 1);
 
@@ -60,6 +62,7 @@ bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub
         if (!_kbhit())
         {
             now = GetTickUs();
+            agvInfo.time = exp_time;
             if (now - last > 1000)
             {
                 lee::reference_cmd_vel(dq_pd, q_p, round_p, cmd_vel_r);
@@ -69,9 +72,10 @@ bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub
                 now = GetTickUs();
                 exp_time = (double)(now - t_start) / 1000000;
                 dt = (double)(now - last) / 1000000;
+                dq_p = (q_p - prev_q_p) / dt;
                 prev_q_p = q_p;
 
-                // Updat the platform's data
+                // Update the platform's data
                 // for translation data
                 for (unsigned int i = 0; i < 2; i++)
                     q_p[i] = platformState.q_p[i];
@@ -102,6 +106,12 @@ bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub
 
                 error_p = q_pd - q_p;
 
+                // Update to ROS
+                agvInfo.agv_axis = {q_p[0], q_p[1], q_p[2]};
+                agvInfo.agv_axisd = {q_pd[0], q_pd[1], q_pd[2]};
+                agvInfo.agv_axisVel = {dq_p[0], dq_p[1], dq_p[2]};
+                agvInfo.agv_axisdVel = {dq_pd[0], dq_pd[1], dq_pd[2]};
+
                 // debug----------------------------
                 cout << "--------------------------" << endl;
                 cout << "error_p:";
@@ -113,7 +123,13 @@ bool platform_control(PlatformState &platformState, ros::Publisher &platform_pub
                 // ROS Publisher
                 geometry_msgs::Twist twist;
                 lee::admittance2platformVel(cmd_vel, twist);
-                platform_pub.publish(twist);
+                platform_cmd_pub.publish(twist);
+                platform_state_pub.publish(agvInfo);
+
+                // Update to ROS
+                agvInfo.cmd_vel = {cmd_vel[0], cmd_vel[1]};
+                agvInfo.twist = {twist.linear.x, twist.angular.z};
+
                 ros::spinOnce();
                 loop_rate.sleep();
             }
@@ -133,13 +149,14 @@ int main(int argc, char **argv)
     // ROS-related
     ros::init(argc, argv, "mobileManipulatorDevice"); // rosnode的名稱
     ros::NodeHandle n;
-    ros::Publisher platform_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);    // the name of the rostopic for publishing
+    ros::Publisher platform_cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);    // the name of the rostopic for publishing
+    ros::Publisher platform_state_pub = n.advertise<kinova_test::agvMsg>("agv_state", 1);
     ros::Subscriber odm_sub = n.subscribe("odom", 1, &PlatformState::updatePlatformData, &platformState);
 
     // Example core
     bool success = true;
     while (success)
-        success &= platform_control(platformState, platform_pub);
+        success &= platform_control(platformState, platform_cmd_pub, platform_state_pub);
 
     if (!success)
         cout << "There has been an unexpected error." << endl;
